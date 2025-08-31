2b6ef5263982e4406aa2e2c04efa3bc031dfaccd
UTKARSHRAIKWAR <utkarshraikwar555@gmail.com>
2025-08-31 23:33:29 +0530

    Notes

--- frontend/src/components/FlashCard.jsx
+++ frontend/src/components/FlashCard.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const FlashCard = () => {
-  return <div>FlashCard</div>;
+  return (
+    <div className="relative flex size-full min-h-screen flex-col bg-[#101323] dark group/design-root overflow-x-hidden font-sans">
+      <div className="layout-container flex h-full grow flex-col"></div>
+    </div>
+  );
 };
 
 export default FlashCard;
--- frontend/src/components/Notes.jsx
+++ frontend/src/components/Notes.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-
-const Notes = () => {
-  return <div>Notes</div>;
-};
-
-export default Notes;
+export default function Notes() {
+  return (
+    <div className="relative flex size-full min-h-screen flex-col bg-[#101323] dark group/design-root overflow-x-hidden font-sans">
+      <div className="layout-container flex h-full grow flex-col"></div>
+    </div>
+  );
+}
