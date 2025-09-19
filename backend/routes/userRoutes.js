fa1255051e0aa395aa4904f74d34aa8e31ecc0f2
UTKARSHRAIKWAR <utkarshraikwar555@gmail.com>
2025-09-19 13:56:15 +0530

    .

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const router = express.Router();
+
 const {
   registerUser,
   authUser,
   allUsers,
 } = require("../controllers/userControllers");
-const { protect } = require("../middlewares/authMiddleware");
 
-const router = express.Router();
+const { protect } = require("../middlewares/authMiddleware");
 
 router.route("/").post(registerUser).get(protect, allUsers);
 router.post("/login", authUser);
